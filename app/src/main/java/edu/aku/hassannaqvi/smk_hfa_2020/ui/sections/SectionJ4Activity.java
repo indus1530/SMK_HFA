6776286b2c6286a98d467f6c263bb75a37198b13
Muhammad Hussain <ahsanpervez74@gmail.com>
2020-10-07 18:09:32 +0500

    Add Sectionj4Activity

---
+++ app/src/main/java/edu/aku/hassannaqvi/smk_hfa_2020/ui/sections/SectionJ4Activity.java
@@ -0,0 +1,108 @@
+package edu.aku.hassannaqvi.smk_hfa_2020.ui.sections;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Clear;
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import edu.aku.hassannaqvi.smk_hfa_2020.R;
+import edu.aku.hassannaqvi.smk_hfa_2020.contracts.FormsContract;
+import edu.aku.hassannaqvi.smk_hfa_2020.core.DatabaseHelper;
+import edu.aku.hassannaqvi.smk_hfa_2020.core.MainApp;
+import edu.aku.hassannaqvi.smk_hfa_2020.databinding.ActivitySectionJ4BindingImpl;
+
+import static edu.aku.hassannaqvi.smk_hfa_2020.core.MainApp.fc;
+import static edu.aku.hassannaqvi.smk_hfa_2020.utils.UtilKt.openSectionMainActivity;
+
+public class Sectionj4Activity extends AppCompatActivity {
+
+    ActivitySectionJ4BindingImpl bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_j4);
+        bi.setCallback(this);
+        setupSkips();
+    }
+
+    private void setupSkips() {
+
+    }
+
+    private boolean UpdateDB() {
+        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+        int updcount = db.updatesFormColumn(FormsContract.FormsTable.COLUMN_SE, fc.getsE());
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, "Updating Database... ERROR!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    private void SaveDraft() throws JSONException {
+    }
+
+    public void BtnContinue() {
+        if (!formValidation()) return;
+        try {
+            SaveDraft();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (UpdateDB()) {
+            finish();
+            startActivity(new Intent(this, Sectionj5Activity.class));
+        } else {
+            Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void BtnEnd() {
+        openSectionMainActivity(this, "E");
+    }
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+    public void showTooltip(@NotNull View view) {
+        if (view.getId() != View.NO_ID) {
+            String package_name = getApplicationContext().getPackageName();
+            String infoid = view.getResources().getResourceName(view.getId()).replace(package_name + ":id/q_", "");
+            int stringRes = this.getResources().getIdentifier(infoid + "_info", "string", getApplicationContext().getPackageName());
+            //String infoText = (String) getResources().getText(stringRes);
+            if (stringRes != 0) {
+                String infoText = (String) getResources().getText(stringRes);
+
+                new AlertDialog.Builder(this)
+                        .setTitle("Info: " + infoid.toUpperCase())
+                        .setMessage(infoText)
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .show();
+            } else {
+                Toast.makeText(this, "No information available on this question.", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(this, "No ID Associated with this question.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+}
